ee2b9267caca9491ada2bfb0ee64c508b2e4865f
ji4l3 <ji4l3@users.noreply.github.com>
2026-07-24 12:48:05 +0800

    Remove unused Expo template files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -103,10 +103,6 @@ function RootLayoutNav() {
       <Stack>
         <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
